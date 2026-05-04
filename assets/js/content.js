1938a98d4b6e5aa150b56ef1f0e3b89e3940b981
petrobsel <160328325+petrobsel@users.noreply.github.com>
2026-05-04 21:23:57 +0200

    Add email contact to socials list

--- assets/js/content.js
+++ assets/js/content.js
@@ -110,6 +110,7 @@ const SITE = {
       { label: "LinkedIn · Petr Obšel",     href: "https://www.linkedin.com/in/petrobsel" },
       { label: "LinkedIn · Daniel Pelánek", href: "https://www.linkedin.com/in/danielpelanek" },
       { label: "Instagram",                 href: "https://www.instagram.com/brndr.app/" },
+      { label: "E-mail: info@brndr.cz",             href: "mailto:info@brndr.cz" },
     ],
   },
 
